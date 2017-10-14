49132b4451ee46fff61abb124a009f900d05ccba
91DarioDev <dariomsn@hotmail.it>
2017-10-14 16:18:48 +0200

    query bugfix

--- src/messages.py
+++ src/messages.py
@@ -11,13 +11,13 @@ def messages(near_interval, far_interval, lang, limit, bot_token):
 			s_ref.title,
 			s_ref.username,
 			s.nsfw
-		FROM messages
+		FROM messages AS m
 		LEFT OUTER JOIN supergroups AS s
 		USING (group_id)
-		LEFT OUTER JOIN supergrousp_ref AS s_ref
+		LEFT OUTER JOIN supergroups_ref AS s_ref
 		USING (group_id)
 		WHERE
-			message_date > (now() - interval %s)
+			m.message_date > (now() - interval %s)
 			AND (s.banned_until IS NULL OR s.banned_until < now()) 
 			AND s.lang = %s
 		GROUP BY group_id, s_ref.title, s_ref.username, s.nsfw
@@ -29,18 +29,18 @@ def messages(near_interval, far_interval, lang, limit, bot_token):
 
 	query_far = """
 		SELECT
-			group_id
-			COUNT (msg_id) AS leaderboard,
+			group_id,
+			COUNT(msg_id) AS leaderboard,
 			s_ref.title,
 			s_ref.username,
 			s.nsfw
-		FROM messages
+		FROM messages AS m
 		LEFT OUTER JOIN supergroups AS s
 		USING (group_id)
-		LEFT OUTER JOIN supergrousp_ref AS s_ref
+		LEFT OUTER JOIN supergroups_ref AS s_ref
 		USING (group_id)
 		WHERE
-			message_date BETWEEN (now() - interval %s) AND (now() - interval %s)
+			m.message_date BETWEEN (now() - interval %s) AND (now() - interval %s)
 			AND (s.banned_until IS NULL OR s.banned_until < now()) 
 			AND s.lang = %s
 		GROUP BY group_id, s_ref.title, s_ref.username, s.nsfw
@@ -48,6 +48,6 @@ def messages(near_interval, far_interval, lang, limit, bot_token):
 		LIMIT %s
 		"""
 
-	far_stats  = db.query_r(query_near, far_interval, near_interval, lang, limit)
+	far_stats = db.query_r(query_far, far_interval, near_interval, lang, limit)
 
 	print(far_stats)
