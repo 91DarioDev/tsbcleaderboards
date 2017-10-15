d7ca45e05bfb971a4608b0e6ab49803cdd04a3e5
91DarioDev <dariomsn@hotmail.it>
2017-10-16 00:00:39 +0200

    ok

--- example.py
+++ example.py
@@ -1,11 +1,16 @@
 """
 this is an example of how to launch a leaderboard
 """
-from src.leaderboards import messages
+from src.leaderboards import messages, members
 import config
 
 messages.messages(near_interval='7 days', 
 					far_interval='14 days', 
 					lang='it', 
 					limit=50,
-					receiver=1234567)
+					receiver=4)
+
+members.members(far_interval='7 days', 
+					lang='it', 
+					limit=50,
+					receiver=4)
