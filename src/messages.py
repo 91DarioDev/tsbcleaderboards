f280eb2f47bc7cca5b908083cf4e3f4003210589
91DarioDev <dariomsn@hotmail.it>
2017-10-15 14:36:39 +0200

    going ahead with the leaderboard message

--- src/messages.py
+++ src/messages.py
@@ -2,6 +2,8 @@
 this file is for the leaderboard ordering groups by amount of messages sent in an interval
 """
 from src import dbwrapper as db
+from src import constants as c
+from src.objects_leaderboard import Leaderboard
 
 def messages(near_interval, far_interval, lang, limit, bot_token):
 	query_near = """
@@ -50,4 +52,43 @@ def messages(near_interval, far_interval, lang, limit, bot_token):
 
 	far_stats = db.query_r(query_far, far_interval, near_interval, lang, limit)
 
-	print(far_stats)
+	leaderboard_list = []
+	count = 0
+	for i in near_stats:
+		count += 1
+		t_id = i[0]
+		t_id = Leaderboard(value=i[1], 
+							position=count, 
+							title=i[2], 
+							username=i[3], 
+							nsfw=i[4])
+		leaderboard_list.append(t_id)
+
+	count = 0
+	for i in far_stats:
+		count += 1
+		t_id = i[0]
+		t_id = Leaderboard(last_value=i[1], last_position=i[2])
+
+
+	message = ""
+	for i in leaderboard_list:
+		i.nsfw = "" if nsfw is False else c.NSFW_E
+		if i.last_value is None:
+			amount = i.value
+			position = ""
+			# check new or back
+		else:
+			amount = "<b>"+str(i.value)+"</b>" if (i.value - i.last_value >= 0) else "<i>"+str(i.value)+"</i>"
+			diff_pos = i.position - i.last_position
+			if diff_pos > 0:
+				position = c.UP_POS_E+"+"+str(diff_pos)
+			elif diff_pos < 0:
+				position = c.DOWN_POS_E+str(diff_pos)
+			else:
+				position = ""
+			position =  if diff_pos > 0
+		message += "{count}) {nsfw}@{username}: {amount}{position_back_new}".format(
+						i.position, nsfw, i.username, amount, position
+			)
+
