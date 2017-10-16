82d510af716b62e6b6d237cfd0e9c6b244dd0fb0
91DarioDev <dariomsn@hotmail.it>
2017-10-16 22:57:01 +0200

    added strings

--- resources/langs/en.py
+++ resources/langs/en.py
@@ -1,3 +1,5 @@
-intro = "First 50 groups that sent more messages in the last 7 days.\n\n"
+intro_messages = "First 50 groups that sent more messages in the last 7 days.\n\n"
+
+intro_members = "First 50 groups having more users.\n\n"
 
 out = "Went out of the leaderboard: "
--- resources/langs/it.py
+++ resources/langs/it.py
@@ -1,3 +1,5 @@
-intro = "Primi 50 gruppi che hanno scambiato più messaggi negli ultimi 7 giorni.\n\n"
+intro_messages = "Primi 50 gruppi che hanno scambiato più messaggi negli ultimi 7 giorni.\n\n"
+
+intro_members = "Primi 50 gruppi più numerosi.\n\n"
 
 out = "Usciti di classifica: "
